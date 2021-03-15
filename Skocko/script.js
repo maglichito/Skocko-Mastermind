720da52bf362647c8852685c2a780505ae50c890
Dzenan Becirovic <63358865+maglichito@users.noreply.github.com>
2021-03-15 18:35:48 +0100

    Update v1.01
    - New Game
    - Fixed bugs
    - Added features

--- Skocko/script.js
+++ Skocko/script.js
@@ -6,6 +6,7 @@ const rightColumn = $$('.column-right');
 const images = $$('.symbol');
 const trash = $('.trash');
 const confirm = $('.confirm');
+const finalCombination = $$('.column-final');
 const randomCombination = new Array(4);
 const resultCombination = new Array(4);
 const userCombination = new Array(4);
@@ -16,28 +17,61 @@ var indexRight = 0;
 var indexBefore = 0;
 var numbersGotPairJ = new Array(4);
 var numbersGotPairI = new Array(4);
+var ifWon = false;
+
+const playAudio = function(){
+        var audio = $(".audio");
+        audio.volume = .3;
+        audio.play();
+}
+// Reset Columns
+const resetColumns = function (column, index) {
+    column[index].style.backgroundImage = "none";
+    column[index].style.opacity = .2;
+    column[index].style.border = "1px solid white";
+    column[index].style.backgroundColor = "rgb(20, 19, 19)";
+}
+
+// New game
+const newGame = function () {
+    indexLeft = 0;
+    indexRight = 0;
+    indexBefore = 0;
+    ifWon = false;
+    clearArray(numbersGotPairJ);
+    clearArray(numbersGotPairI);
+    for (i = 0; i < leftColumn.length; i++) {
+        resetColumns(leftColumn, i);
+        resetColumns(rightColumn, i);
+    }
+    for (i = 0; i < finalCombination.length; i++) {
+        resetColumns(finalCombination, i);
+    }
+    generateCombination();
+}
 
 // Adding symbols
-const addItem = function(symbol){
-    if(indexLeft < indexBefore+4){
-        leftColumn[indexLeft].style.opacity = 1;
-        leftColumn[indexLeft].style.backgroundRepeat = "norepeat";
-        leftColumn[indexLeft].style.backgroundSize = "cover";
-        leftColumn[indexLeft].style.backgroundColor = "rgba(171, 183, 183, 1)";
-        leftColumn[indexLeft].style.border = "none";    
-        leftColumn[indexLeft].style.backgroundImage = "url('images/" + symbol +  ".png')";
-        indexLeft++;   
+const addItem = function (column, symbol, index) {
+    if (indexLeft < indexBefore + 4) {
+        column[index].style.opacity = 1;
+        column[index].style.backgroundRepeat = "norepeat";
+        column[index].style.backgroundSize = "cover";
+        column[index].style.backgroundColor = "rgba(171, 183, 183, 1)";
+        column[index].style.border = "none";
+        column[index].style.backgroundImage = "url('images/" + symbol + ".png')";
     }
 }
 
 // Deleting symbols
-const deleteItem = function(){
-    if(indexLeft > indexBefore){
-        leftColumn[indexLeft-1].style.backgroundImage = "none";
-        leftColumn[indexLeft-1].style.opacity = .3;
-        leftColumn[indexLeft-1].style.backgroundColor = "rgb(20, 19, 19)";
+const deleteItem = function () {
+    if(ifWon === true){
+        alert("Igra je zavresna zapocnite novu igru!");
+        return;
+    }
+    if (indexLeft > indexBefore) {
+        resetColumns(leftColumn, indexLeft - 1)
         indexLeft--;
-    }else{
+    } else {
         console.log("All elements are already deleted.");
         alert("Obrisao si vec sve elemente!");
     }
@@ -50,95 +84,152 @@ const deleteItem = function(){
 // 3 - Club
 // 4 - Spade
 // 5 - Star
-const generateCombination = function(){
+const generateCombination = function () {
     for (i = 0; i < randomCombination.length; i++) {
         let randomNumber = Math.floor(Math.random() * 6);
         randomCombination[i] = randomNumber;
-      }
-      console.log(randomCombination);
+    }
+    console.log(randomCombination);
 }
-
+const fillFinalCombination = function () {
+    for (i = 0; i < finalCombination.length; i++) {
+        switch (randomCombination[i]) {
+            case 0:
+                addItem(finalCombination, "smiley", i);
+                break;
+            case 1:
+                addItem(finalCombination, "heart", i);
+                break;
+            case 2:
+                addItem(finalCombination, "diamond", i);
+                break;
+            case 3:
+                addItem(finalCombination, "clubs", i);
+                break;
+            case 4:
+                addItem(finalCombination, "spade", i);
+                break;
+            case 5:
+                addItem(finalCombination, "star", i);
+                break;
+        }
+    }
+}
 // Comparing random generating and user combination
-const comparingCombinations = function(){
-
-    if(indexLeft < indexBefore+4){
+const comparingCombinations = function () {
+    if(ifWon === true){
+        alert("Igra je zavresna zapocnite novu igru!");
+        return;
+    }
+    if (indexLeft < indexBefore + 4) {
         alert("Popunite sva polja!");
         return;
     }
     console.log(userCombination);
     for (i = 0; i < userCombination.length; i++) {
         for (j = 0; j < randomCombination.length; j++) {
-            if(userCombination[i] === randomCombination[j]){
-                   if(i === j && numbersGotPairJ[j] != true && numbersGotPairI[i] !=true){
-                        numbersGotPairJ[j] = true;
-                        numbersGotPairI[i] = true;
-                        break;
-                   }else if(numbersGotPairJ[j] == null && numbersGotPairI[i] == null){
-                        numbersGotPairJ[j] = false;
-                        break;
-                   }           
+            if (userCombination[i] === randomCombination[j]) {
+                if (i === j && numbersGotPairJ[j] != true && numbersGotPairI[i] != true) {
+                    numbersGotPairJ[j] = true;
+                    numbersGotPairI[i] = true;
+                    break;
+                } else if (numbersGotPairJ[j] == null && numbersGotPairI[i] == null) {
+                    numbersGotPairJ[j] = false;
+                    break;
+                }
             }
-          }
-      }
-      paintRightRow();
+        }
+    }
+    paintRightRow();
+    if (indexLeft == leftColumn.length) {
+        fillFinalCombination();
+    }
 }
-const clearArray = function(Array){
+// Used to clear array
+const clearArray = function (Array) {
     for (i = 0; i < Array.length; i++) {
         Array[i] = null;
-      }
-}
-const paint = function(color,index){
-            rightColumn[index].style.backgroundColor = color;
-            rightColumn[index].style.opacity = 1;
-            rightColumn[index].style.border = "none";
-}
-const paintRightRow = function (){
-    let colored = indexRight;
-    for (i = 0; i < resultCombination.length; i++) {
-        if(numbersGotPairJ[i] === true){
-            paint("red",colored);
-            colored++;
-        }
-      }
-      for (i = 0; i < resultCombination.length; i++) {
-        if(numbersGotPairJ[i] === false){
-            paint("yellow",colored);
-            colored++;
-        }
-      }
-      clearArray(numbersGotPairJ);
-      clearArray(numbersGotPairI);
-      indexBefore+= 4;
-      indexRight+= (4-colored)+colored;
+    }
 }
 
+// Paint right side columns
+const paint = function (color, index) {
+    rightColumn[index].style.backgroundColor = color;
+    rightColumn[index].style.opacity = 1;
+    rightColumn[index].style.border = "none";
+}
+const paintRightRow = function () {
+    let coloredRed = 0;
+    let colored = indexRight;
+    for (i = 0; i < resultCombination.length; i++) {
+        if (numbersGotPairJ[i] === true) {
+            paint("red", colored);
+            coloredRed++;
+            colored++;
+        }
+    }
+    for (i = 0; i < resultCombination.length; i++) {
+        if (numbersGotPairJ[i] === false) {
+            paint("yellow", colored);
+            colored++;
+        }
+    }
+    clearArray(numbersGotPairJ);
+    clearArray(numbersGotPairI);
+    indexBefore += 4;
+    indexRight += (4 - colored) + colored;
+    if (coloredRed === 4) {
+        fillFinalCombination();
+        ifWon = true;
+        return;
+    }
+}
+// Adding events to all images
 images.forEach(element => {
-    element.addEventListener('click', ()=>{
-        switch(parseInt(element.getAttribute("value"))){
-            case 0:
-                addItem("smiley");
-                userCombination[indexLeft-1-indexBefore] = parseInt(element.getAttribute("value"));
-                break;
-            case 1:
-                addItem("heart");
-                userCombination[indexLeft-1-indexBefore] = parseInt(element.getAttribute("value"));
-                break;
-            case 2:
-                addItem("diamond");
-                userCombination[indexLeft-1-indexBefore] = parseInt(element.getAttribute("value"));
-                break;
-            case 3:
-                addItem("clubs");
-                userCombination[indexLeft-1-indexBefore] = parseInt(element.getAttribute("value"));
-                break;
-            case 4:
-                addItem("spade");
-                userCombination[indexLeft-1-indexBefore] = parseInt(element.getAttribute("value"));
-                break;
-            case 5:
-                addItem("star");
-                userCombination[indexLeft-1-indexBefore] = parseInt(element.getAttribute("value"));
-                break;
+    element.addEventListener('click', () => {
+        if(ifWon === true){
+            alert("Igra je zavresna zapocnite novu igru!");
+            return;
+        }
+        if (indexLeft != leftColumn.length) {
+            switch (parseInt(element.getAttribute("value"))) {
+                case 0:
+                    addItem(leftColumn, "smiley", indexLeft);
+                    indexLeft++;
+                    userCombination[indexLeft - 1 - indexBefore] = parseInt(element.getAttribute("value"));
+                    playAudio();
+                    break;
+                case 1:
+                    addItem(leftColumn, "heart", indexLeft);
+                    indexLeft++;
+                    userCombination[indexLeft - 1 - indexBefore] = parseInt(element.getAttribute("value"));
+                    playAudio();
+                    break;
+                case 2:
+                    addItem(leftColumn, "diamond", indexLeft);
+                    indexLeft++;
+                    userCombination[indexLeft - 1 - indexBefore] = parseInt(element.getAttribute("value"));
+                    playAudio();
+                    break;
+                case 3:
+                    addItem(leftColumn, "clubs", indexLeft);
+                    indexLeft++;
+                    userCombination[indexLeft - 1 - indexBefore] = parseInt(element.getAttribute("value"));
+                    playAudio();
+                    break;
+                case 4:
+                    addItem(leftColumn, "spade", indexLeft);
+                    indexLeft++;
+                    userCombination[indexLeft - 1 - indexBefore] = parseInt(element.getAttribute("value"));
+                    playAudio();
+                    break;
+                case 5:
+                    addItem(leftColumn, "star", indexLeft);
+                    indexLeft++;
+                    userCombination[indexLeft - 1 - indexBefore] = parseInt(element.getAttribute("value"));
+                    playAudio();
+                    break;
+            }
         }
     })
 });
